310a4928781884a09bc3db151b8d473ccd04b4e3
ganpeng <190875565@qq.com>
2023-08-08 22:40:42 +0800

    wip

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 'use strict';
-const alfy = require('alfy');
+import alfy from "alfy";
+import tailwindJson from './tailwindcss.json' assert { type: "json" };
 
-alfy.output([
-	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
-]);
+const cssPorperties = Object.keys(tailwindJson);
+
+const matches = alfy.matches(alfy.input, cssPorperties);
+
+
+const items = matches.reduce((acc, porperty) => {
+	const twObj = tailwindJson[porperty];
+	Object.keys(twObj).forEach((tw) => {
+		acc.push({
+			title: `${tw}: ${Object.keys(twObj[tw]).map(css => `${css}: ${twObj[tw][css]}`).join('; ')}`,
+		})
+	})
+	return acc;
+}, []);
+
+alfy.output(items);
